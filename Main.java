88396862c832dd2fc52228f8e8eef7095cb564df
apoa30 <borgybp+3@gmail.com>
2025-12-15 17:08:16 -0800

    corresponding grade to letter grade

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String args[]) {
-        
+      int grade = 90;
+      String letterGrade;
+      
+      if (grade >= 90) {
+        letterGrade = "A";
+      } else if (grade >= 80) {
+        letterGrade = "B";
+      } else if (grade >= 70) {
+        letterGrade = "C";
+      } else if (grade >= 60) {
+        letterGrade = "D";
+      } else {
+        letterGrade = "F";
+      }
+      System.out.println(letterGrade);
     }
 }
