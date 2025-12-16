87e7e341d99cf7f37a4e0285106b2d7a4646e5e3
AdamHM-tech <1051392@etusd.org>
2025-12-15 17:16:01 -0800

    made grade function

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String args[]) {
+        int grade = 100;
         
+        if (grade < 60) {
+            System.out.println("F");
+        }
+        else if (grade >= 60 && grade < 70) {
+            System.out.println("D");
+        }
+        else if (grade >= 70 && grade < 80) {
+            System.out.println("C");
+        }
+        else if (grade >= 80 && grade < 90) {
+            System.out.println("B");
+        }
+        else if (grade >= 90 && grade <= 100) {
+            System.out.println("A");
+        }
     }
 }
